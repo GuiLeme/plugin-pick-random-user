907b25643705f8097203fd448ffa911ff7722399
Guilherme Leme <leme.guilherme.p@gmail.com>
2025-07-28 15:56:36 -0300

    [fix-no-notification] fix notification should not appear once the user has already seen the picked user

--- src/components/modal/component.tsx
+++ src/components/modal/component.tsx
@@ -6,6 +6,7 @@ import * as Styled from './styles';
 import { PickUserModalProps, WindowClientSettings } from './types';
 import { PickedUserViewComponent } from './picked-user-view/component';
 import { PresenterViewComponent } from './presenter-view/component';
+import hasCurrentUserSeenPickedUser from '../../utils/utils';
 
 const intlMessages = defineMessages({
   currentUserPicked: {
@@ -64,9 +65,17 @@ export function PickUserModal(props: PickUserModalProps) {
 
   useEffect(() => {
     // Play audio when user is selected
+
+    const hasCurrentUserSeen = hasCurrentUserSeenPickedUser(
+      pickedUserSeenEntries,
+      userId,
+      pickedUserWithEntryId?.pickedUser?.userId,
+    );
     const isPingSoundEnabled = !isPluginSettingsLoading && pluginSettings?.pingSoundEnabled;
     if (isPingSoundEnabled && pickedUserWithEntryId
-      && pickedUserWithEntryId?.pickedUser?.userId === userId) {
+      && pickedUserWithEntryId?.pickedUser?.userId === userId
+      // Current user must not have seen this entry and data should be done loading
+      && !hasCurrentUserSeen && !pickedUserSeenEntries?.loading) {
       const { cdn, basename } = window.meetingClientSettings.public.app;
       const host = cdn + basename;
       const pingSoundUrl: string = pluginSettings?.pingSoundUrl
@@ -76,7 +85,7 @@ export function PickUserModal(props: PickUserModalProps) {
       audio.play();
       notifyRandomlyPickedUser(intl.formatMessage(intlMessages.currentUserPicked));
     }
-  }, [userId, pickedUserWithEntryId]);
+  }, [userId, pickedUserWithEntryId, pickedUserSeenEntries]);
 
   useEffect(() => {
     setShowPresenterView(currentUser?.presenter && !pickedUserWithEntryId);
