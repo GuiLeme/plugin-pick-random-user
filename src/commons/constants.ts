b2ccc16c7675e95db1d2cc64a4e87cbcc6a7cc4e
Guilherme Leme <leme.guilherme.p@gmail.com>
2026-03-09 14:41:34 -0300

    [lock-countdown-change] Change lock countdown span to 1 second

--- src/commons/constants.ts
+++ src/commons/constants.ts
@@ -4,4 +4,4 @@ export const TIMEOUT_CLOSE_NOTIFICATION = 5000;
 
 export const DEFAULT_PING_SOUND_URL = 'resources/sounds/doorbell.mp3';
 
-export const DEFAULT_PREVENT_CLOSE_DELAY_SECONDS = 3; // seconds
+export const DEFAULT_PREVENT_CLOSE_DELAY_SECONDS = 1; // seconds
